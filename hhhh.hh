1656d98626e10eddc51283f86c2ec9c530fb43a0
Abdelali Kacimi <abkacimi@e3r4p12.1337.ma>
2025-06-24 15:48:39 +0100

    main

--- hhhh.hh
+++
@@ -1,5 +0,0 @@
-/*
-git add .
-git commit "hhhhhhhhhhhh"
-git push
-*/
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <vector>
+
+class user
+{
+	private :
+		std::string name;
+		std::string password;
+	public :
+};
+
+class irc
+{
+	private :
+		std::vector <user> users;
+		std::string password;
+		int port;
+	public :
+		irc(int port, std::string password);
+};
+
+irc::irc(int port, std::string password) : port(port), password(password)
+{
+	sockaddr_in serv_add;
+	std::memset(&serv_add, 0, sizeof(serv_add));
+	serv_add.sin_family = AF_INET;//which type of ip (ip4/ ip6)
+	serv_add.sin_addr.s_addr = INADDR_ANY;
+	serv_add.sin_port = htons(port);
+}
+
+int main (int ac , char **av)
+{
+	if (ac == 3)
+	{
+		int port = std::atoi(av[1]);
+		std::string password = av[2];
+		irc server(port, password);
+	}
+}
