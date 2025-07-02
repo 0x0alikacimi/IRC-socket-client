3fa756b82a1eb50ebbd1c7690a1575add099ba74
Abdelali Kacimi <abkacimi@e3r4p14.1337.ma>
2025-07-02 13:58:10 +0100

    master

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #include <fcntl.h>
 #include <poll.h>
 
+#define DEF_PROTOCOL 0 /*the default protocol for IPv4 TCP, which is TCP itself.*/
+#define MAX_PENDING 1 /*is the backlog queue size (max pending connections waiting).*/
+#define BLOCK_WAIT -1 /*blocks indefinitely until at least one socket has some activity*/
+
 class User
 {
 	private :
@@ -52,7 +56,6 @@ User *Irc::look_for(int fd)
 	return (NULL);
 }
 
-
 void Irc::start_server()
 {
 	std::cout << "srever is lestenning to port : " << port << "\n" << std::endl;
@@ -65,7 +68,12 @@ void Irc::start_server()
 
 	while (true)
 	{
-		int check_poll = poll(pfds.data(), pfds.size(), -1);
+		int check_poll = poll(pfds.data(), pfds.size(), BLOCK_WAIT);
+		if (check_poll < 0)
+		{
+			std::cerr << "pll " << std::endl;
+			break;
+		}
 		if (pfds[0].revents & POLLIN)
 		{
 			int new_client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);/*!*/
@@ -74,11 +82,12 @@ void Irc::start_server()
 				std::cerr << "error accepting new client." << std::endl;
 				continue;
 			}
-			fcntl(new_client_fd, F_SETFL, O_NONBLOCK);
-			std::cout << "new client connected!" << "\n" << std::endl;
+			/*fcntl(new_client_fd, F_SETFL, O_NONBLOCK);*/
 			struct pollfd new_pfd; new_pfd.fd = new_client_fd; new_pfd.events = POLLIN; new_pfd.revents = 0;
 			pfds.push_back(new_pfd);
+			/*now I gotta check the password and the rest of data before accepting him or not */
 			users.push_back(User(new_client_fd));
+			std::cout << "new client connected!" << "\n" << std::endl;
 		}
 
 		int i = 1;
@@ -99,6 +108,7 @@ void Irc::start_server()
 				else
 				{
 					buff[n] = '\0';
+
 					std::cout << "received form " << i << ": " << buff << std::endl;
 				}
 			}
@@ -110,7 +120,7 @@ void Irc::start_server()
 
 Irc::Irc(int port, std::string password) : port(port), password(password)
 {
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	server_fd = socket(AF_INET, SOCK_STREAM, DEF_PROTOCOL);
 
 	fcntl(server_fd, F_SETFL, O_NONBLOCK);
 
@@ -122,16 +132,35 @@ Irc::Irc(int port, std::string password) : port(port), password(password)
 
 	bind(server_fd, (sockaddr *)&serv_add, sizeof(serv_add));
 
-	listen(server_fd, 1);
+	listen(server_fd, MAX_PENDING);
 }
 
 int main (int ac , char **av)
 {
 	if (ac == 3)
 	{
-		int port = std::atoi(av[1]);
-		std::string password = av[2];
-		Irc server(port, password);
-		server.start_server();
+		try
+		{
+			int port = std::atoi(av[1]);
+			if (port <= 0 || port >= 65535)
+			{
+				std::cerr << "invalid port number" << std::endl;
+				return 1;
+			}
+			std::string password = av[2];
+			if (password.size() < 3)
+			{
+				std::cerr << "invalid password " << std::endl;
+				return 1;
+			}
+			Irc server(port, password);
+			server.start_server();
+		}
+		catch(std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+			return (1);
+		}
+		return (0);
 	}
 }
