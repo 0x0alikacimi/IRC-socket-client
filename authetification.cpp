9835a912ed79a657af77c28989d7c2a5914d08c6
Hatim Alouani <haalouan@e3r4p14.1337.ma>
2025-07-13 16:14:16 +0100

    push

--- authetification.cpp
+++ authetification.cpp
@@ -5,6 +5,10 @@
 bool Server::isReadyForRegistration(std::vector<std::string> tokens, PendingClient* pending) const{
 	std::string pwd = password;
 	pending->handleRegistration(tokens, pwd, users, pending_users);
+	if (!pending->get_password_set() || !pending->get_password_valid()){
+		sendReply(pending->get_fd(), ERR_PASSWDMISMATCH(std::string("")));
+		return false;
+	}
 	if (pending->get_username_set() && pending->get_nickname_set() && pending->get_password_set()  && pending->get_nickname_valid() && pending->get_password_valid() && pending->get_hostname_set() && pending->get_servername_set() && pending->get_realname_set())
 		return true;
 	return false;
@@ -29,11 +33,13 @@ void PendingClient::handleRegistration(std::vector<std::string> tokens, std::str
 	else if (command == "PASS" && tokens.size() == 2) {
 		handlePassCommand(value, password);
 	}
-	else if (command == "USER" && tokens.size() <= 5) {
-		if (tokens.size() == 5){
+	else if (command == "USER") {
+		if (tokens.size() >= 5){
 			std::string value1 = parsse(tokens[2]);
 			std::string value2 = parsse(tokens[3]);
 			std::string value3 = parsse(tokens[4]);
+			for (size_t i = 5; i < tokens.size(); ++i)
+				value3 += " " + parsse(tokens[i]);
 			handleUserCommand(value, value1, value2, value3, users, pendingUsers);
 		}
 		else
@@ -74,6 +80,7 @@ void PendingClient::handlePassCommand(std::string& password, std::string& truePa
 	setPassword(password);
 	if (isPasswordSet())
 		this->password_set = true;
-	if (checkPassword(password, truePassword))
+	if (checkPassword(password, truePassword)){
 		this->password_valid = true;
+	}
 }
